1e65a2f7275629f3656e026f0ee71a64ae583ab0
orhaninac <orhaninc@outlook.com>
2022-01-22 16:38:05 +0300

    Lombok @Data @AllArgsConstructor @NoArgsConstructor @Log4j2 @Builder

--- PtaikaDev/src/main/java/com/patika/PtaikaDevApplication.java
+++ PtaikaDev/src/main/java/com/patika/PtaikaDevApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PtaikaDevApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(PtaikaDevApplication.class, args);
     }
 
