85f12d66d5bbb39c42fb1cd8ac6ad072b0143e6e
orhaninac <orhaninc@outlook.com>
2022-01-22 15:18:20 +0300

    Initial commit

---
+++ PtaikaDev/src/main/java/com/patika/PtaikaDevApplication.java
@@ -0,0 +1,13 @@
+package com.patika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtaikaDevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PtaikaDevApplication.class, args);
+    }
+
+}
